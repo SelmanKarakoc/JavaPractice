079fca1153e903790501dcfb113000c4eb7ad2dd
SelmanKarakoc <selman_karakoc@yahoo.com>
2023-01-16 10:25:43 +0300

    first commit

---
+++ src/test/day01/Q1_Print.java
@@ -0,0 +1,20 @@
+package test.day01;
+
+public class Q1_Print {
+    // "Hello "\ / 'World'" konsolda yazdır
+
+    public static void main(String[] args) {
+         /*
+	  \n (or \r)  : Starts from a new line : Metni bir alt satırdan itibaren yazdırır.
+	          \t  : Horizontal tab         : Metni yatayda 1 tab miktarı kaydırır.
+	          \\  : Prints a back slash    : \  Ters slash yazdırır
+	          \'  : Prints single quote    : '  Tek tırnak yazdırır.
+	          \"  : Prints double quote    : "  Çift tırnak yazdırır.
+	 */
+
+        System.out.println("\"Hello \"\\ / 'World'\"");
+
+
+
+    }
+}
---
+++ src/test/day01/Q2_Print.java
@@ -0,0 +1,25 @@
+package test.day01;
+
+public class Q2_Print {
+     /*
+        ************************************
+                "ATM'ye Hosgeldiniz"
+             1- Bakiye Sorgulama
+             2- Para Yatirma
+             3- Para Cekme
+             4- Menu'den Cikis
+        *************************************
+         */
+     public static void main(String[] args) {
+         System.out.println("************************************");
+         System.out.println("\t   \"ATM'ye Hoşgeldiniz\"");
+         System.out.println("\t1- Bakiye Sorgulama");
+         System.out.println("\t2- Para Yatirma");
+         System.out.println("\t3- Para Cekme");
+         System.out.println("\t4- Menu'den Cikis");
+         System.out.println("************************************");
+
+
+
+     }
+}
