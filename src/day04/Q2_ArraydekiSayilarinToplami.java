b79ee962edb045a3c6aa3134064b6946e7f38ecb
SelmanKarakoc <selman_karakoc@yahoo.com>
2023-01-18 10:09:04 +0300

    4.gun sonu

---
+++ src/day04/Q2_ArraydekiSayilarinToplami.java
@@ -0,0 +1,35 @@
+package day04;
+
+import java.sql.Array;
+import java.util.Arrays;
+
+public class Q2_ArraydekiSayilarinToplami {
+    //Create an array of 5 floats and calculate their sum.
+    public static void main(String[] args) {
+
+        float[] arr1=new float[5];
+
+        arr1[0]=0.5f;
+        arr1[1]=2.5f;
+        arr1[2]=2.7f;
+        arr1[3]=3.8f;
+        arr1[4]=4.9f;
+
+        System.out.println(Arrays.toString(arr1));
+
+        float toplam=0;
+
+        for (int i = 0; i < arr1.length; i++) {
+            toplam+=arr1[i];
+
+        }
+
+        System.out.println("Array deki sayiların toplamı= "+ toplam);
+
+
+
+
+
+
+    }
+}
